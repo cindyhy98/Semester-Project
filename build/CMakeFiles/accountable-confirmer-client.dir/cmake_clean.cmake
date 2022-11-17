08c687e12454fc59fd1bd24610573baef01566ec
Yu-Hsin Yang <cindy02017@gmail.com>
2022-11-17 16:30:05 +0100

    Test the functionality of ac.cpp in main.cpp

---
+++ build/CMakeFiles/accountable-confirmer-client.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/accountable-confirmer-client.dir/main_client.cpp.o"
+  "CMakeFiles/accountable-confirmer-client.dir/main_client.cpp.o.d"
+  "CMakeFiles/accountable-confirmer-client.dir/src/ac.cpp.o"
+  "CMakeFiles/accountable-confirmer-client.dir/src/ac.cpp.o.d"
+  "CMakeFiles/accountable-confirmer-client.dir/src/ac_bls.cpp.o"
+  "CMakeFiles/accountable-confirmer-client.dir/src/ac_bls.cpp.o.d"
+  "CMakeFiles/accountable-confirmer-client.dir/src/ecdsa_pki.cpp.o"
+  "CMakeFiles/accountable-confirmer-client.dir/src/ecdsa_pki.cpp.o.d"
+  "CMakeFiles/accountable-confirmer-client.dir/src/message.cpp.o"
+  "CMakeFiles/accountable-confirmer-client.dir/src/message.cpp.o.d"
+  "CMakeFiles/accountable-confirmer-client.dir/src/socket.cpp.o"
+  "CMakeFiles/accountable-confirmer-client.dir/src/socket.cpp.o.d"
+  "accountable-confirmer-client"
+  "accountable-confirmer-client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/accountable-confirmer-client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/accountable-confirmer-client.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for accountable-confirmer-client.
+# This may be replaced when dependencies are built.
