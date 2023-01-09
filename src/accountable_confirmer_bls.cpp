46f9808ea7462df89fc34bae2c6b7a0352b7a576
Yu-Hsin Yang <cindy02017@gmail.com>
2023-01-09 20:14:09 +0100

    Delete useless comment

--- src/accountable_confirmer_bls.cpp
+++ src/accountable_confirmer_bls.cpp
@@ -18,8 +18,6 @@ namespace accountable_confirmer_bls {
 
         // use the latest eth2.0 spec
         blsSetETHmode(BLS_ETH_MODE_LATEST);
-
-//        printf("[accountable_confirmer_bls::Init] Successful! \n");
     }
 
     void KeyGen(Key* k) {
@@ -28,15 +26,11 @@ namespace accountable_confirmer_bls {
 
         // get PublicKey pub from SecretKey sec
         blsGetPublicKey(&k->pub, &k->sec);
-
-//        printf("[accountable_confirmer_bls::KeyGen] Generate PublicKey from SecretKey\n");
     }
 
 
     void Sign(Key* k, blsSignature* sig, const char* msg) {
-
         blsSign(sig, &k->sec, msg, strlen(msg));
-//        printf("[accountable_confirmer_bls::Sign] Make a Signature\n");
     }
 
     /* Return 1 if it is valid else 0 */
@@ -46,7 +40,6 @@ namespace accountable_confirmer_bls {
 
     void AggSign(blsSignature *aggSig, const blsSignature *sigVec, mclSize n) {
         blsAggregateSignature(aggSig, sigVec, n);
-//        printf("[accountable_confirmer_bls::AggSign] Make a Aggregate Signature\n");
     }
 
     /* Return 1 if it is valid else 0 */
